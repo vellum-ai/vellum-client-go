8febbc7fb288585f9707a1b2bd2cfb433973247d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-11-22 17:43:35 +0000

    SDK regeneration

--- core/request_option.go
+++ core/request_option.go
@@ -57,7 +57,7 @@ func (r *RequestOptions) cloneHeader() http.Header {
 	headers := r.HTTPHeader.Clone()
 	headers.Set("X-Fern-Language", "Go")
 	headers.Set("X-Fern-SDK-Name", "github.com/vellum-ai/vellum-client-go")
-	headers.Set("X-Fern-SDK-Version", "v0.9.14")
+	headers.Set("X-Fern-SDK-Version", "v0.0.1843")
 	return headers
 }
 
