e49c58fd6ca4c08cbbde702df06acc976222ad66
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-15 14:42:55 +0000

    SDK regeneration

--- prompts/client.go
+++ prompts/client.go
@@ -8,12 +8,12 @@ import (
 	json "encoding/json"
 	errors "errors"
 	fmt "fmt"
-	io "io"
-	http "net/http"
-
 	vellumclientgo "github.com/vellum-ai/vellum-client-go"
 	core "github.com/vellum-ai/vellum-client-go/core"
 	option "github.com/vellum-ai/vellum-client-go/option"
+	io "io"
+	http "net/http"
+	os "os"
 )
 
 type Client struct {
@@ -24,8 +24,8 @@ type Client struct {
 
 func NewClient(opts ...option.RequestOption) *Client {
 	options := core.NewRequestOptions(opts...)
-	if options.ApiVersion == nil || *options.ApiVersion == "" {
-		options.ApiVersion = core.GetDefaultApiVersion()
+	if options.ApiVersion == "" {
+		options.ApiVersion = os.Getenv("VELLUM_API_VERSION")
 	}
 	return &Client{
 		baseURL: options.BaseURL,
