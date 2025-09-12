e3b317188663357f841d4cae7bce521d222f4c58
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-12 22:13:03 +0000

    SDK regeneration

--- option/request_option.go
+++ option/request_option.go
@@ -3,6 +3,7 @@
 package option
 
 import (
+	vellumclientgo "github.com/vellum-ai/vellum-client-go"
 	core "github.com/vellum-ai/vellum-client-go/core"
 	http "net/http"
 	url "net/url"
@@ -71,7 +72,7 @@ func WithApiKey(apiKey string) *core.ApiKeyOption {
 }
 
 // WithApiVersion sets the apiVersion request header.
-func WithApiVersion(apiVersion *core.ApiVersionEnum) *core.ApiVersionOption {
+func WithApiVersion(apiVersion *vellumclientgo.ApiVersionEnum) *core.ApiVersionOption {
 	return &core.ApiVersionOption{
 		ApiVersion: apiVersion,
 	}
